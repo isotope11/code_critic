ab6867b0b92dc73921f7fa87e71773820ac10642
Robby Clements <rclements@isotope11.com>
2012-07-14 18:09:02 -0500

    removed validateable from users model

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,8 +2,8 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :token_authenticatable, :confirmable,
   # :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :recoverable, :rememberable, :trackable, :validatable, :omniauthable, :omniauth_providers => [:xrono]
+  devise  :recoverable, :rememberable, :trackable, :omniauthable, :omniauth_providers => [:xrono]
 
   # Setup accessible (or protected) attributes for your model
-  attr_accessible :email, :password, :password_confirmation, :remember_me
+  attr_accessible :email, :remember_me
 end
