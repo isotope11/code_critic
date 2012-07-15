5743afd837f3c2db61fd38be21dc9964a58fd2c7
Robby Clements <rclements@isotope11.com>
2012-07-14 22:09:10 -0500

    added review request mail template

--- app/models/contact_mailer.rb
+++ app/models/contact_mailer.rb
@@ -1,10 +1,10 @@
 class ContactMailer < ActionMailer::Base
-  def review_request(review_request, email)
+  def review_request(rr, email)
     default_url_options[:host]
     recipients email
     subject    'You have a review request that needs attention'
     from       'code-critic-noreply@isotope11.com'
     sent_on    Time.now
-    body      review_request 
+    @review_request = rr
   end
 end
