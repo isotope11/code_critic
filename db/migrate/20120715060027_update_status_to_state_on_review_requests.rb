9399fc8ddc2214709bf3926cb63f01fc51c022e4
Josh Adams <josh@isotope11.com>
2012-07-15 01:47:17 -0500

    I hate myself

--- db/migrate/20120715060027_update_status_to_state_on_review_requests.rb
+++ db/migrate/20120715060027_update_status_to_state_on_review_requests.rb
@@ -1,6 +1,6 @@
 class UpdateStatusToStateOnReviewRequests < ActiveRecord::Migration
   def change
-    #remove_column :review_requests, :status
+    remove_column :review_requests, :status
     add_column :review_requests, :state, :string, :default => 'review_created'
   end
 end
