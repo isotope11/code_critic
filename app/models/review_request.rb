9a43ec92c053c451acb0b14792435cc56fc3e7cb
Robby Clements <rclements@isotope11.com>
2012-07-15 00:43:24 -0500

    added status to review requests and added state machine states to the model

--- app/models/review_request.rb
+++ app/models/review_request.rb
@@ -2,4 +2,14 @@ class ReviewRequest < ActiveRecord::Base
   belongs_to :reviewable, :polymorphic => true
   belongs_to :user
   belongs_to :commit
+
+  state_machine :state, :initial => :review_created do
+
+    event :review_completed do
+      transition :review_created => :review_completed
+    end
+
+    state :review_created
+    state :review_completed
+  end
 end
