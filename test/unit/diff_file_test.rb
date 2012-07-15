64e87b9bef33a7415bf6d7d58fccfec33a7e2cfb
Josh Adams <josh@isotope11.com>
2012-07-15 04:42:30 -0500

    Add some more interesting diff styling

---
+++ test/unit/diff_file_test.rb
@@ -0,0 +1,27 @@
+require 'test_helper'
+
+describe DiffFile do
+  before do
+    @str = <<-EOL
+--- /dev/null
++++ b/app/assets/stylesheets/_commits.css.scss
+@@ -1 +1,69 @@
++.commits{
++  margin: 0 auto;
++}
+    EOL
+    git_diff = mock()
+    git_diff.expects(:diff).returns(@str)
+    @diff_file = DiffFile.new(git_diff)
+  end
+
+  it "serves each line up as a DiffLine for #each_line" do
+    @diff_file.each_line do |l|
+      l.must_be_instance_of(DiffLine)
+    end
+  end
+
+  it "knows its file path" do
+    @diff_file.file_path.must_equal "/app/assets/stylesheets/_commits.css.scss"
+  end
+end
