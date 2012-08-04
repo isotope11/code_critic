fd2cbdbae3b6723b1da237d49b312a01eaa950e6
Josh Adams <josh@isotope11.com>
2012-08-03 23:16:30 -0500

    Fix pagination for multi-branch

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -5,7 +5,7 @@ module ApplicationHelper
     git_repo.commits(branch, PAGE_SIZE, current_paginated_commit_index)
   end
 
-  def commit_pagination_links repo
+  def commit_pagination_links repo, branch='master'
     git_repo = repo.git_repo
     commit_count = git_repo.commit_count
     total_pages = (commit_count / PAGE_SIZE).ceil
@@ -13,7 +13,7 @@ module ApplicationHelper
     html << "<div class='pagination'>"
     html << "<ul>"
     (1...total_pages).each do |page|
-      link = link_to(page, "?page=#{page}")
+      link = link_to(page, "?page=#{page}&branch=#{branch}")
       if current_paginated_commit_page == page
         html << "<li class='active'>#{link}</li>"
       else
